791ed73546a5f3c57580007d9b05d894645f2038
boeani05 <flyingdoge@gmx.at>
2025-10-29 10:52:53 +0100

    Mockdata now loads the rooms instead of the cinema class as this way, it makes more sense

--- src/main/java/com/egger/cinema/Mockdata.java
+++ src/main/java/com/egger/cinema/Mockdata.java
@@ -3,12 +3,14 @@ package com.egger.cinema;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -62,6 +64,74 @@ public class Mockdata {
             }
             return List.copyOf(result);
         }
+    }
+
+    public static void loadData(AbstractCinema cinema) {
+        final Map<String, Room> rooms;
+        final List<Movie> movies;
+        final List<CinemaEvent> events;
+        final Map<String, Double> snacks;
+        final Map<String, Double> boughtSnacks;
+
+        snacks = new LinkedHashMap<>();
+        snacks.put("Popcorn", 5.99);
+        snacks.put("Soda", 1.99);
+        snacks.put("Nachos", 3.99);
+
+        try (InputStream moviesJson = Mockdata.class.getResourceAsStream("/movies.json")) {
+
+            movies = Mockdata.loadMovies(moviesJson);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        rooms = new TreeMap<>();
+        events = new ArrayList<>();
+
+        boughtSnacks = new LinkedHashMap<>();
+
+        Movie m1 = randomlySelectMovie(movies);
+        Movie m2 = randomlySelectMovie(movies);
+        Movie m3 = randomlySelectMovie(movies);
+        Movie m4 = randomlySelectMovie(movies);
+
+        CinemaEvent e1 = new CinemaEvent(m1, null, LocalDateTime.now().plusHours(2));
+        CinemaEvent e2 = new CinemaEvent(m2, null, LocalDateTime.now().plusHours(3));
+        CinemaEvent e3 = new CinemaEvent(m3, null, LocalDateTime.now().plusHours(4));
+        CinemaEvent e4 = new CinemaEvent(m4, null, LocalDateTime.now().plusHours(5));
+
+        Room a = new Room(m1, 10, 7, "A", e1);
+        Room b = new Room(m2, 5, 5, "B", e2);
+        Room c = new Room(m3, 10, 10, "C", e3);
+        Room d = new Room(m4, 5, 10, "D", e4);
+
+        e1.setRoom(a);
+        e2.setRoom(b);
+        e3.setRoom(c);
+        e4.setRoom(d);
+
+        rooms.put("A", a);
+        rooms.put("B", b);
+        rooms.put("C", c);
+        rooms.put("D", d);
+
+        events.addAll(List.of(e1, e2, e3, e4));
+
+        DiscountCode d1 = new DiscountCode(1, 0.0);
+        DiscountCode d2 = new DiscountCode(2, 2.99);
+        DiscountCode d3 = new DiscountCode(3, 1.99);
+        DiscountCode d4 = new DiscountCode(4, 1.49);
+
+        final Map<Integer, Double> discount = new HashMap<>(Map.of(d1.code(), d1.discount(), d2.code(), d2.discount(), d3.code(), d3.discount(), d4.code(), d4.discount()));
+
+        cinema.setData(rooms, movies, events, discount, snacks);
+
+    }
+
+    public static Movie randomlySelectMovie(List<Movie> movies) {
+        Random random = new Random();
+        int index = random.nextInt(movies.size());
+        return movies.get(index);
     }
 
     private static String firstNonBlank(String a, String b) {
